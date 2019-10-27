738f91065791c8a7caadf05346b2004c8bd8ab9f
Arnold Kibira <arnold.ril3y@gmail.com>
2019-10-27 20:32:28 +0300

    create the profile model

--- jirani/models.py
+++ jirani/models.py
@@ -1,10 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+import PIL
+from PIL import Image
 
 # Create your models here.
 
 
-class neighbourhood(models.Model):
+class Neighbourhood(models.Model):
     name = models.CharField(max_length=20)
     location = models.CharField(max_length=20)
     occupants = models.IntegerField()
      
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pic = models.ImageField(default = 'default.jpg', upload_to ='profile_pics')
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return f'{self.user.username}\'s Profile'
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        img = PIL.Image.open(self.pic.path)
+
+        if img.height > 300 or img.width >300:
+            output_size = (152,152)
+            img.thumbnail(output_size)
+            img.save(self.pic.path)
