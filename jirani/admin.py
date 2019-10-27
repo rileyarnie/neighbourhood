6eee8c2f99124bab56b7624fffd6d3979692b908
Arnold Kibira <arnold.ril3y@gmail.com>
2019-10-27 21:44:03 +0300

    register all models so that they can be visible in the admin page

---
+++ jirani/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Neighbourhood, Profile, Business
+
+# Register your models here.
+admin.register.site(Neighbourhood)
+admin.register.site(Profile)
+admin.register.site(Business)
