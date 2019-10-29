c04ca10b8eb21cc508ac24cadb3866ef01022429
Arnold Kibira <arnold.ril3y@gmail.com>
2019-10-29 16:14:26 +0300

    create the register view

--- jirani/views.py
+++ jirani/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'jirani/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request, f'Account created! Log In Now!')
+            return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
