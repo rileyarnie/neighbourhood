f71b4d3ab7f3b08a121b3c91f76b0bb38266ff5b
Arnold Kibira <arnold.ril3y@gmail.com>
2019-10-30 07:50:59 +0300

    update the urls to include the list view template

--- jirani/urls.py
+++ jirani/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from .views import UpdateListView
 
 urlpatterns = [
     path('', views.index, name='index' ),
     path('register/', views.register, name ='register'),
     path('home/', views.home, name='home' ),
-    path('search/', views.search_results, name='search_results')
-
+    path('search/', views.search_results, name='search_results'),
+    path('news/', UpdateListView.as_view(), name='newsy'),
+ 
    
 ]
